9ba954c70676d7f1401a774258400549caa61278
H3BC <konstanty.koszewski@gmail.com>
2016-06-06 02:05:05 +0200

    next updates

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,6 +1,6 @@
 var player = 1;
 
-	var btn1 = document.getElementById('btn1').innerHTML;
+	
 boxed = function(e){
 	var btn = e.target;
 	
@@ -16,13 +16,29 @@ boxed = function(e){
 		document.getElementById(btn.id).innerHTML = 'O';
 		player--;
 	}
-	checking();
+	ifWin();
 	
 }
 
+// CLEANING ALL MARKED FIELDS 
 resetFields = function(){
 	for( var n = 1; n <= 9; n++){
 		document.getElementById("btn" +n).innerHTML = null;
+	}
+}
+
+ifWin = function(){
+	var resault = checking();
+	if (resault == 1){
+		alert("'X' Wins!")
+		resetFields();
+	}
+	else if(resault == -1){
+		alert("'O' Wins!");
+		resetFields();
+	}
+	else{
+		return 0;
 	}
 }
 
@@ -30,18 +46,32 @@ checking = function(){
 	var btn1 = document.getElementById('btn1').innerHTML;
 	var btn2 = document.getElementById('btn2').innerHTML;
 	var btn3 = document.getElementById('btn3').innerHTML;
-	var a = ifWin(btn1,btn2,btn2);
-	console.log(a);
+	var btn4 = document.getElementById('btn4').innerHTML;
+	var btn5 = document.getElementById('btn5').innerHTML;
+	var btn6 = document.getElementById('btn6').innerHTML;
+	var btn7 = document.getElementById('btn7').innerHTML;
+	var btn8 = document.getElementById('btn8').innerHTML;
+	var btn9 = document.getElementById('btn9').innerHTML;
+	
+	return rowCheck(btn1,btn2,btn3) 
+	+ rowCheck(btn4,btn5,btn6) 
+	+ rowCheck(btn7,btn8,btn9) 
+	+ rowCheck(btn1,btn4,btn7) 
+	+ rowCheck(btn2,btn5,btn8) 
+	+ rowCheck(btn3,btn6,btn9) 
+	+ rowCheck(btn1,btn5,btn9) 
+	+ rowCheck(btn3,btn5,btn7);
 	
 }
 
 
-ifWin = function(a, b, c){
+// CHECKING IF SOMEONE FULLFILLED CONDITIONS TO WIN
+rowCheck = function(a, b, c){
  if (a === 'X' && b === "X" && c == 'X'){
  	return 1;
  }
  else  if (a === 'O' && b === "O" && c == 'O'){
- 	return 2;
+ 	return -1;
  }
  else{
  	return 0;
